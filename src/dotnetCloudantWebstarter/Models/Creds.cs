036c0f190cc925e346f1e1815b35176c32c1cd1d
vietanh1996 <anhnvph04091@fpt.edu.vn>
2016-12-18 21:16:45 +0700

    123

--- src/dotnetCloudantWebstarter/Models/Creds.cs
+++
@@ -1,9 +0,0 @@
-namespace CloudantDotNet.Models
-{
-    public class Creds
-    {
-        public string username { get; set; }
-        public string password { get; set; }
-        public string host { get; set; }
-    }
-}
